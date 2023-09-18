3dc03842c6057a845b145c508f11df216ee4004e
Jahkaan Wray <gammawray26@gmail.com>
2023-09-18 17:44:54 +0100

    Update user function

--- src/helperFunctions/userFunctions.ts
+++ src/helperFunctions/userFunctions.ts
@@ -1,5 +1,11 @@
-export async function getUsers() {
-    const res = await fetch(`http://localhost:8080/users`);
+export async function getUsers(requestBody: any) {
+    const res = await fetch(`http://localhost:8080/users/list`, {
+        method: "POST",
+        body: JSON.stringify(requestBody),
+        headers: {
+            "content-type": "application/json",
+        },
+    });
     const data = await res.json();
     return data;
 }
